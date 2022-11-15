39a52a9847ead3a49b383a884701321d4d55c1ec
Carla Pico <carlaspico@gmail.com>
2022-11-15 11:21:32 -0500

    added some styling to the caclulator border and the input box

--- src/App.js
+++ src/App.js
@@ -10,40 +10,40 @@ function App() {
         <h1>Carla's Calculator</h1>
         <body className='calculator'>
           <section className='inputBar'>
-            <input type="button" value="0" />
+            <div>{total}</div>
           </section>
 
           <section className='topRow'>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>/</button>
+            <button onClick={()=>{setTotal(total + 7)}}>7</button>
+            <button onClick={()=>{setTotal(total + 8)}}>8</button>
+            <button onClick={()=>{setTotal(total + 9)}}>9</button>
+            <button >/</button>
           </section>
           
           <section className='secondRow'>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
+            <button onClick={()=>{setTotal(total + 4)}}>4</button>
+            <button onClick={()=>{setTotal(total + 5)}}>5</button>
+            <button onClick={()=>{setTotal(total + 6)}}>6</button>
             <button>x</button>
           </section>
 
           <section className='thirdRow'>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
+            <button onClick={()=>{setTotal(total + 1)}}>1</button>
+            <button onClick={()=>{setTotal(total + 2)}}>2</button>
+            <button onClick={()=>{setTotal(total + 3)}}>3</button>
             <button>+</button>
           </section>
 
           <section className='bottomRow'>
-            <button>0</button>
-            <button>C</button>
+            <button onClick={()=>{setTotal(total + 0)}}>0</button>
+            <button onClick={()=>{setTotal(0)}}>C</button>
             <button>.</button>
             <button>-</button>
           </section>
         </body>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/carlapico/carla-calculator"
           target="_blank"
           rel="noopener noreferrer"
           >
